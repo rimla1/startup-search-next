ed1f6a9e8e6dbe71bc98c87a7ac7244922b7fcd8
Almir Muminovic <projectalmir@gmail.com>
2024-11-19 11:49:30 +0100

    Creating Heading section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <>
-    Home
-   </>
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
+      </section>
+    </>
   );
 }
