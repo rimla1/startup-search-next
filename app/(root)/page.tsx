5a5cce314d8e642fdd05eaa7d3d0d457154cc77f
Almir Muminovic <projectalmir@gmail.com>
2024-11-20 11:03:53 +0100

    Creating Startup Card UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,50 @@
+import StartupCard from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 
-export default async function Home({searchParams}: {
-  searchParams: Promise<{query?: string}>
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
 }) {
-
-  const query = (await searchParams).query
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: 'Rimla'
+      },
+      _id: 1,
+      description: "This is the description",
+      image: "https://scx2.b-cdn.net/gfx/news/hires/2019/3-robot.jpg",
+      category: "Robots",
+      title: "We robots",
+    },
+  ];
 
   return (
     <>
       <section className='pink_container'>
-        <h1 className='heading'>Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
-        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.</p>
-        <SearchForm query={query}/>
+        <h1 className='heading'>
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Search Results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className='mt-7 card_grid'>
+          {posts.length > 0 ? (posts.map((post: StartupCardType, index: number) => (
+            <StartupCard key={post?._id} post={post}/>
+          )
+
+          )) : (<p className='no-results'>No startups found</p>)}
+        </ul>
       </section>
     </>
   );
